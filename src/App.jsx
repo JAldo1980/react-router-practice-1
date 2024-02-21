8417d674218d143abf61b3f23969ee98def888e8
JAldo1980 <jamesalderman1980@gmail.com>
2024-02-21 13:25:00 +0000

    simple routing practice with components

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Home from "./component/Home";
+import About from "./component/About";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5 }}>
+          Home
+        </Link>
+        <Link to="/about" style={{ padding: 5 }}>
+          About
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
